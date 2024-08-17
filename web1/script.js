dc04ec0b7237de4be5b99c701c936afade9c6c0e
minhtien2200002455 <2200002455@nttu.edu.vn>
2024-08-17 22:52:56 +0700

    Web profile

---
+++ web1/script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('CONTACT BUTTON').addEventListener('click', function() {
+        alert('CẢM ƠN BẠN ĐÃ LIÊN HỆ! VUI LÒNG GỌI SỐ 03894567823.');
+    });
+})
+
+
